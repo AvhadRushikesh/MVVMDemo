28abea27831c6eb25ad9dc4b5e15e7e3f6cbd189
rushi-smb <rushi@smbios.co.in>
2023-02-27 17:56:07 +0530

    Passing Parameter to Command - SearchBar

--- MVVMDemo/MVVM/ViewModels/CommandsViewModel.cs
+++ MVVMDemo/MVVM/ViewModels/CommandsViewModel.cs
@@ -10,12 +10,18 @@ namespace MVVMDemo.MVVM.ViewModels
     public class CommandsViewModel
     {
         public ICommand ClickCommand { get; }
+        public ICommand SearchCommand { get; }
+        public string SearchTerm { get; set; }
         public CommandsViewModel()
         {
             ClickCommand = new Command(() =>
             {
                 App.Current.MainPage
                 .DisplayAlert("Title", "Message", "OK");
+            });
+            SearchCommand = new Command((s) =>
+            {
+                var data = s;
             });
         }
     }
