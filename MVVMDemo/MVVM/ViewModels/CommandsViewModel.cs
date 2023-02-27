2fe3a02d15b66acff01b3511faa989eba184e37a
rushi-smb <rushi@smbios.co.in>
2023-02-27 17:39:17 +0530

    Create Command - Display Alert

---
+++ MVVMDemo/MVVM/ViewModels/CommandsViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MVVMDemo.MVVM.ViewModels
+{
+    public class CommandsViewModel
+    {
+        public ICommand ClickCommand =>
+            new Command(() =>
+            App.Current.MainPage
+            .DisplayAlert("Title", "message", "OK"));
+    }
+}
